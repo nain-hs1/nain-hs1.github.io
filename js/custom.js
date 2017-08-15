5b70e0f69cd0a0fe15ffe99348266cab3c6f3818
nain-hs1 <himanshu.singh@education.nsw.gov.au>
2017-08-15 18:47:54 +1000

    final

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+(function ($) {
+	$(document).ready(function () {
+
+		/* menu visiable js start */
+
+		$(".menu-appander").click(function () {
+			$(".mega-menu").addClass("menupop");
+			$(this).css("opacity", "0");
+		})
+		$(".menu-cross").click(function () {
+			$(".mega-menu").removeClass("menupop");
+			$(".menu-appander").css("opacity", "1");
+		})
+		/* menu visiable js end */
+
+		/* src box pop up js start */
+		$(".search-icon").click(function () {
+			$(".search-box").addClass("srcpop");
+			$(this).css("opacity", "0");
+		})
+		$(".src-hidden").click(function () {
+			$(".search-box").removeClass("srcpop");
+			$(".search-icon").css("opacity", "1");
+		})
+		/* src box pop up js end */
+
+		// slider_carousel js start
+		$('#slider_carousel').owlCarousel({
+			loop: true,
+			margin: 0,
+			nav: true,
+			autoplay: false,
+			autoplayTimeout: 5000,
+			autoplayHoverPause: false,
+			animateOut: 'fadeOut',
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 1
+				},
+				1000: {
+					items: 1
+				}
+			}
+		})
+		// slider_carousel js end
+
+
+
+
+	});
+})(jQuery);
